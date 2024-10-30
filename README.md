a75afc94f7b7046eb3184e769ca17c315a3c3677
Yatin W <98123821+Wahi-Yatin-1104@users.noreply.github.com>
2024-10-30 01:10:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PumpChestTheGame
+
+Developed with Unreal Engine 5
---
+++ Source/PumpChestTheGame/PumpChestTheGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PumpChestTheGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APumpChestTheGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APumpChestTheGameGameMode();
+};
+
+
+
